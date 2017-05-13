a5677f4dd96e31b9123fe817fd34601fd6b4ea12
jillsophie <jillphys@hotmail.com>
2017-05-13 16:05:38 +0100

    Use aliases for Pixi API

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 import Pixi from 'pixi.js';
 import scaleToWindow from './helpers/scaleToWindow.js';
 
+// Aliases
+let Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
 //Create renderer by autodetecting whether to use WebGL or Canvas Drawing API to render graphics
 // This creates a new canvas html tag
-let renderer = PIXI.autoDetectRenderer(800, 400);
+let renderer = autoDetectRenderer(800, 400);
 
 // scale canvas to window
 //let scale = scaleToWindow(renderer.view);
@@ -12,15 +19,15 @@ let renderer = PIXI.autoDetectRenderer(800, 400);
 document.getElementById('root').appendChild(renderer.view);
 
 // Create a (main/root) container object called the stage
-let stage = new PIXI.Container();
+let stage = new Container();
 
 // Load an image and call setup function after loading
-PIXI.loader.add('images/cat.png').load(setup);
+loader.add('images/cat.png').load(setup);
 
 // This setup function will run when the image has loaded
 function setup() {
   //Create a sprite from the texture
-  let cat = new PIXI.Sprite(PIXI.loader.resources['images/cat.png'].texture);
+  let cat = new Sprite(resources['images/cat.png'].texture);
 
   // Add sprite to the stage
   stage.addChild(cat);
