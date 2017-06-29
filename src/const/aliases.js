35101b004364db74cc1770b44fc317f8ba807575
jillsophie <jillphys@hotmail.com>
2017-06-29 21:43:12 +0100

    Refactor with App class

---
+++ src/const/aliases.js
@@ -0,0 +1,10 @@
+import Pixi from 'pixi.js';
+
+export const autoDetectRenderer = PIXI.autoDetectRenderer;
+export const Container = PIXI.Container;
+export const loader = PIXI.loader;
+export const resources = PIXI.loader.resources;
+export const Sprite = PIXI.Sprite;
+export const TilingSprite = PIXI.extras.TilingSprite;
+export const AnimatedSprite = PIXI.extras.AnimatedSprite;
+export const Text = PIXI.Text;
---
+++ src/const/gameConstants.js
@@ -0,0 +1,7 @@
+export const rendererWidth = 910;
+export const rendererHeight = 512;
+export const playerStartX = 232;
+export const playerStartY = 256;
+export const gravity = 0.05;
+export const wingPower = -2.5;
+export const numberOfPillars = 15;
